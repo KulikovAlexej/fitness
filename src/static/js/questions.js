9a9621bbeb3ee498c7a5f8184c192e8052d62368
KulikovAlexej <aleksey.kulikov.96@gmail.com>
2018-05-08 18:39:03 +0300

    add content in ask part, fixed ask position

--- src/static/js/questions.js
+++ src/static/js/questions.js
@@ -1,30 +1,7 @@
-// class QuestionBoard {
-// 	constructor(){
-// 		this.counter = 0;
-// 	}
-	
-// }
-
-// class Tab {
-// 	constructor(contentTab){
-// 		this.tab = contentTab;
-// 	}
-// 	toggleContent() {
-// 		console.log(this.tab);
-// 		this.tab.classList.toggle('active');
-// 		if(this.tab.classList.contains('active')){
-// 			this.tab.style.maxHeight = `${this.tab.scrollHeight}px`;
-// 		}
-// 		else{
-// 			this.tab.style.maxHeight = '0px';
-// 		}
-		
-// 	}
-// }
-
+console.log(1234);
 document.querySelectorAll('.toggle_btn').forEach((btn, i) => {
 	btn.addEventListener('click', (event) => {
-		
+		console.log(123)
 		let currentNode = event.target;
 		while (!currentNode.classList.contains('question_wrap')) {
 			currentNode = currentNode.parentNode;
@@ -33,14 +10,3 @@ document.querySelectorAll('.toggle_btn').forEach((btn, i) => {
 		tab.toggleContent();
 	})
 })
-
-// document.querySelector('.content').addEventListener( 'wheel', scrollingBlock );
-// /// скролл вниз должен работать только при условии того, что 
-// function scrollingBlock ( event ) {
-
-// 	let container = document.querySelector('.content .tabs_wrap');
-
-// 	console.log( container.getBoundingClientRect().bottom )
-// 	console.log( window.innerHeight )
-// 	console.log( document.documentElement.clientHeight )
-// }
