fe7020132d8731dfc479e7bcde0b31718b459034
KulikovAlexej <aleksey.kulikov.96@gmail.com>
2018-05-08 17:14:55 +0300

    cannot position ask dashboard

--- src/static/js/questions.js
+++ src/static/js/questions.js
@@ -1,26 +1,26 @@
-class QuestionBoard {
-	constructor(){
-		this.counter = 0;
-	}
+// class QuestionBoard {
+// 	constructor(){
+// 		this.counter = 0;
+// 	}
 	
-}
+// }
 
-class Tab {
-	constructor(contentTab){
-		this.tab = contentTab;
-	}
-	toggleContent() {
-		console.log(this.tab);
-		this.tab.classList.toggle('active');
-		if(this.tab.classList.contains('active')){
-			this.tab.style.maxHeight = `${this.tab.scrollHeight}px`;
-		}
-		else{
-			this.tab.style.maxHeight = '0px';
-		}
+// class Tab {
+// 	constructor(contentTab){
+// 		this.tab = contentTab;
+// 	}
+// 	toggleContent() {
+// 		console.log(this.tab);
+// 		this.tab.classList.toggle('active');
+// 		if(this.tab.classList.contains('active')){
+// 			this.tab.style.maxHeight = `${this.tab.scrollHeight}px`;
+// 		}
+// 		else{
+// 			this.tab.style.maxHeight = '0px';
+// 		}
 		
-	}
-}
+// 	}
+// }
 
 document.querySelectorAll('.toggle_btn').forEach((btn, i) => {
 	btn.addEventListener('click', (event) => {
@@ -34,13 +34,13 @@ document.querySelectorAll('.toggle_btn').forEach((btn, i) => {
 	})
 })
 
-document.querySelector('.content').addEventListener( 'wheel', scrollingBlock );
-/// скролл вниз должен работать только при условии того, что 
-function scrollingBlock ( event ) {
+// document.querySelector('.content').addEventListener( 'wheel', scrollingBlock );
+// /// скролл вниз должен работать только при условии того, что 
+// function scrollingBlock ( event ) {
 
-	let container = document.querySelector('.content .tabs_wrap');
+// 	let container = document.querySelector('.content .tabs_wrap');
 
-	console.log( container.getBoundingClientRect().bottom )
-	console.log( window.innerHeight )
-	console.log( document.documentElement.clientHeight )
-}
+// 	console.log( container.getBoundingClientRect().bottom )
+// 	console.log( window.innerHeight )
+// 	console.log( document.documentElement.clientHeight )
+// }
