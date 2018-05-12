f4d2375c44b7333c88c03bbdb0b2da584bb2fc63
KulikovAlexej <aleksey.kulikov.96@gmail.com>
2018-05-12 19:01:32 +0300

    add all adaptive

---
+++ src/static/js/wards.js
@@ -0,0 +1,17 @@
+let sliderContainer = document.querySelector('.slider_wrap');
+let counter_slides = document.querySelector('.pagin_wrap.wards');
+let wards_slider = new WardsSlider(sliderContainer, counter_slides);
+
+wards_slider.init();
+wards_slider.createPageCounter();
+
+document.querySelector('#wards_onright').addEventListener('click', ( event ) => {
+
+	if ( wards_slider.activeSlide == wards_slider.amount_slides - 1 && wards_slider.notAnimated ) {
+		wards_slider.restart()
+	}
+	else{
+		wards_slider.moveSlide('forward');
+	}
+
+})
