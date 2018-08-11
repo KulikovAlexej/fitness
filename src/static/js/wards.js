c5d9658792934b078cf24f7a390e1f9c6db82f2b
KulikovAlexej <aleksey.kulikov.96@gmail.com>
2018-08-11 19:00:57 +0300

    add sliders-scripts, add simple structure

--- src/static/js/wards.js
+++ src/static/js/wards.js
@@ -2,6 +2,7 @@ let sliderContainer = document.querySelector('.slider_wrap');
 let counter_slides = document.querySelector('.pagin_wrap.wards');
 let wards_slider = new WardsSlider(sliderContainer, counter_slides);
 
+console.log(1234)
 wards_slider.init();
 wards_slider.createPageCounter();
 
